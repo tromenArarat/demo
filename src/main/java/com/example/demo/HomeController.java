e0dc01c4b15b775a2ff6615b941f7947fd5ce27c
Tomas Manoukian <tomanoukian@gmail.com>
2023-09-20 22:28:35 -0300

    configure data source

--- src/main/java/com/example/demo/HomeController.java
+++ src/main/java/com/example/demo/HomeController.java
@@ -11,6 +11,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HomeController {
     @GetMapping({"/","/home","/status"})
     public String getStatus(){
-        return "FUncaaa";
+        return "index.html";
     }
 }
